5d0667105b6a8ec19d05652cfc750b768563d5d9
Stef Horner <shorner@wearearchitect.com>
2014-01-23 18:50:02 +0000

    add some figlet

--- servers/ubuntu-apache-php.sh
+++ servers/ubuntu-apache-php.sh
@@ -5,16 +5,25 @@ sudo apt-get upgrade -y
 
 sudo apt-get install -y build-essential g++ libssl-dev figlet curl vim git-core ntp ufw php5 apache2 apache2-utils libapache2-mod-php5 mysql-server php5-mysql memcached php5-memcached beanstalkd php5-json php5-curl php5-gd php5-xmlrpc php5-intl php5-mcrypt ruby1.9.1-dev postfix
 
+figlet installing node
+
 curl https://raw.github.com/creationix/nvm/master/install.sh | sh
 source ~/.profile
 nvm install 0.10.24
 nvm alias default 0.10.24
 
+figlet node modules
+
 sudo npm install -g grunt-cli
 sudo npm install -g bower
+
+
+figlet "compass & sass"
+
 sudo gem install sass --no-ri --no-rdoc
 sudo gem install compass --no-ri --no-rdoc
 
+figlet enable mods
 
 a2enmod rewrite
 a2enmod ssl
@@ -27,13 +36,20 @@ php5enmod gd
 php5enmod xmlrpc
 php5enmod intl
 
+figlet firewall
+
 ufw allow http
 ufw allow https
 ufw allow ssh
 
 ufw enable
 
+figlet ntp
+
 ntpdate ntp.ubuntu.com
 service ntp start
+
+
+figlet reboot
 
 sudo reboot
